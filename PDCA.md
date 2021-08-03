b104450fa914d31d6eca52a1872800e670e7acdd
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-03 19:17:21 +0800

    success

---
+++ PDCA.md
@@ -0,0 +1,23 @@
+isNumberinRange
+P - 3mins 
+D - 1:38:24
+C - get pressured by time
+A - time get me pressured but nevertheless gets the work in time
+
+isEndBiggerThanStart
+P - 2mins
+D - 1:04:29
+C - Can easily understand what is the requirement
+A - Believed that this is the most basic part of the project
+
+generate Expression:
+P - 10
+D - 13:48:29
+C - have a hard time finding the error that cause infinite loop
+A - need to reconstruct the whole nested for loop
+
+generateMultiplicationLine
+P - 5
+D - 4:49:58
+C - Improve critical thinking
+A - Tried to search the syntax for string replace
--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -4,4 +4,59 @@ public class MultiplicationTable {
     public String create(int startNumber, int endNumber) {
         return null;
     }
+
+
+
+    public void main(String[] args) {
+        validation obj1 = new validation();
+        obj1.isNumberinRange(2,4);
+        obj1.isNumberinRange(2,4);
+        generateMultiplicationTable obj2 = new generateMultiplicationTable();
+        String Line = obj2.generateMultiplicationExpression(2,4);
+        obj2.generatemultiplicationLine(Line);
+
+    }
+class validation
+{
+    boolean isNumberinRange(int startNumber, int endNumber)
+    {
+        boolean inRange=false;
+        if((startNumber>0 && endNumber>0) && (startNumber<1001 && endNumber <1001))
+        {
+                inRange=true;
+        }
+        return inRange;
+    }
+    boolean isEndBiggerThanStart(int startNumber, int endNumber)
+    {
+        boolean isEndBiggerThanStart=false;
+        if(startNumber<endNumber)
+        {
+            isEndBiggerThanStart=true;
+        }
+        return isEndBiggerThanStart;
+    }
+
+}
+class generateMultiplicationTable
+{
+    String generateMultiplicationExpression(int startNumber, int endNumber)
+    {
+        String multiplicationExpression="";
+        for (int i = startNumber; i <= endNumber; i++) {
+            for (int j = startNumber; j <= i; j++) {
+                System.out.print(j + "*" + i + " = " + j * i );
+                multiplicationExpression+=j + "*" + i + " = " + j * i;
+            }
+            multiplicationExpression+=".";
+        }
+        return multiplicationExpression;
+    }
+    void generatemultiplicationLine(String multiplicationExpression)
+    {
+        multiplicationExpression = multiplicationExpression.replace(".","\n");
+    }
+
+}
+
 }
